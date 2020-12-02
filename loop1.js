9011f5d1f4303977a9220488b19d257c760d05bf
Stephen-prog <stev.monye04@gmail.com>
2020-12-01 23:41:20 -1100

    i solved loop1 alghorithm

---
+++ loop1.js
@@ -0,0 +1,16 @@
+
+ 
+function multiply(x,end) {
+    
+    for (let i = 1; i <= end; i++) {
+        
+        result = i * x;
+        console.log(`${i} * ${x} = ${result}`)
+        
+    }
+    
+}
+multiply(2,12) ; 
+
+
+
