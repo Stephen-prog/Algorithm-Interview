7811a05f99af25a1fd00a461c3e5e8f021a82e1c
Stephen-prog <stev.monye04@gmail.com>
2020-12-18 00:07:29 -1100

    made some changes

---
+++ decision-ans.js
@@ -0,0 +1,23 @@
+let x = 67;
+if (x >= 70) {
+    console.log('A')
+};
+if (x < 70 && x > 59) {
+    console.log('B'); 
+}; 
+if (x < 60 && x > 49) {
+    console.log('C'); } 
+
+if (x < 50 && x > 44) {
+    console.log('D');
+}; 
+
+if (x < 45 && x > 39) {
+    console.log('E'); 
+};
+
+if (x <=39 ) {
+    console.log('F'); 
+}; 
+
+
---
+++ switch.js
@@ -0,0 +1,18 @@
+
+let state = 'anambra';
+switch (state.toLowerCase()) {
+    case 'abia':
+        console.log('Umuahia');
+        console.log('Gods own State');
+        break;
+    case 'anambra':
+        console.log('Awka');
+        console.log('Home for all');
+        break;
+    case 'akwa ibom':
+        console.log('Uyo');
+        console.log('Land of promise');
+        break;    
+    default:
+        console.log('State in Nigeria')
+}
